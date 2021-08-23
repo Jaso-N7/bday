c019be68fdca7aa8502071ec9108ffabd0b935f5
Jason S. Robinson <json.robinson@gmail.com>
2021-08-22 20:43:31 -0500

    Implemented a version of `ONE-OF'. Confirmed test system correctly setup.

--- tests/csv-test.lisp
+++ tests/csv-test.lisp
@@ -1,5 +1,43 @@
 (defpackage bday/tests.csv
   (:documentation "Property-Based and Unit Tests for the CSV Parsing package.")
-  (:use :cl))
+  (:use :cl :cl-quickcheck)
+  (:export #:sample-test))
 
 (in-package :bday/tests.csv)
+
+(eval-when (:execute :compile-toplevel :load-toplevel)
+  (defmacro one-of (&rest exprs)
+    "Courtesy of Paul Graham's ANSI CL - Macros pg 170."
+    `(case (random, (length exprs))
+       ,@(let ((key -1))
+	   (mapcar #'(lambda (expr)
+		       `(,(incf key) ,expr))
+		   exprs)))))
+
+;;; PROPERTIES
+(defun sample-test ()
+  (named "Dummy Test"
+	 (is= 1 1)))
+
+
+;;; HELPERS
+
+(defparameter textdata
+  (concatenate 'string 
+	       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	       ":;<=>?@ !#$%&'()*+-./\\^_`{}"))
+
+(defun field ()
+   (one-of (unquoted-text) (quotable-text)))
+
+(defun unquoted-text ()
+  "Used to generate data that will require no known escape sequence once in it once converted."
+  (error "Not yet implemented"))
+
+(defun quotable-text ()
+  "Used to generate sequences that may possibly require escaping (the four escapable characters
+ are only present in this one)."
+  (error "Not yet implemented."))
+
+
+;;; GENERATORS
--- tests/suite.lisp
+++ tests/suite.lisp
@@ -3,16 +3,18 @@
   (:use :cl
         :bday
         :cl-quickcheck)
+  (:import-from :bday/tests.csv
+		#:sample-test)
   (:export #:run))
 
 (in-package :bday/tests.suite)
 
 (defparameter *skipped*
-  (vector nil)
+  (vector NIL)
   "Do not run these tests.")
 
 (defparameter *suite*
-  (vector nil)
+  (vector #'sample-test)
   "Test Suite. Holds the names of all the tests to be run.")
 
 (defun run ()
